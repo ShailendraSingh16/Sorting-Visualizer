980ca594588b8ecfadab8ef7fbe4ba0a40b75924
Shailendra Singh <75472269+ShailendraSingh16@users.noreply.github.com>
2023-10-03 14:53:40 +0530

    Update merge_sort.js

--- scripts/merge_sort.js
+++ scripts/merge_sort.js
@@ -1,9 +1,4 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
+ 
 
 function Merge()
 {
@@ -72,10 +67,4 @@ function merge_partition(start,end)
         merge_sort(start,mid,end);
     }
 }
-
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
+ 
